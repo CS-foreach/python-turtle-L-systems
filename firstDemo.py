502ab98ad4682dec51857f5d2166655f9c8ba78c
phentos <40075433+phentos@users.noreply.github.com>
2022-04-16 21:09:20 -0700

    Create firstDemo.py

---
+++ firstDemo.py
@@ -0,0 +1,26 @@
+from turtle import *
+
+moveDistance = 20
+turnAmount = 60
+
+def goForward(): forward(moveDistance)
+
+def turnLeft(): left(turnAmount)
+
+def turnRight(): right(turnAmount)
+
+actions = {
+    "f":goForward,
+    "+":turnLeft,
+    "-":turnRight
+}
+
+def runThis(structure):
+    for _ in structure: actions[_]()
+
+reset()
+
+for _ in range(6):
+    runThis("f+")
+
+exitonclick()
