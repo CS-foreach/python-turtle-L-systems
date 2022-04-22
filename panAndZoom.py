f2d19ca1434292ec48edf91a1b8620b15dfc08c7
phentos <40075433+phentos@users.noreply.github.com>
2022-04-21 23:45:10 -0700

    Create panAndZoom.py

---
+++ panAndZoom.py
@@ -0,0 +1,48 @@
+from turtle import *
+from tkinter import ALL
+
+def centerScreenCanvasCoords(x=0, y=0):
+    screenWidth = screensize()[0]
+    screenHeight = screensize()[1]
+
+    x = (x + (screenWidth - window_width())/2)/screenWidth
+    y = (y + (screenHeight + window_height())/2)/screenHeight
+
+    return (x, y)
+
+def doZoom(factor):
+    x, y = centerScreenCanvasCoords()
+    x = getcanvas().canvasx(x)
+    y = getcanvas().canvasy(y)
+    getcanvas().scale(ALL, x, y, factor, factor)
+
+def zoomIn():
+    doZoom(1.15)
+
+def zoomOut():
+    doZoom(.85)
+
+def panLeft():
+    getcanvas().xview_scroll(-1, "units")
+
+def panRight():
+    getcanvas().xview_scroll(1, "units")
+
+def panUp():
+    getcanvas().yview_scroll(-1, "units")
+
+def panDown():
+    getcanvas().yview_scroll(1, "units")
+
+def setListens():
+    onkeypress(bye, "q")
+    onkeypress(panUp, "w")
+    onkeypress(panLeft, "a")
+    onkeypress(panDown, "s")
+    onkeypress(panRight, "d")
+    onkeypress(zoomIn, "Up")
+    onkeypress(zoomOut, "Down")
+
+def initialize():
+    setListens()
+    listen()
